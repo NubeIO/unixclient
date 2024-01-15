fda215a4cfe3906bc85f73d3105e3dd998dfaed2
NubeDev <aidan@enviroservices.com.au>
2024-01-15 14:50:14 +1100

    added demo

---
+++ demo/demo.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/NubeIO/unixclient"
+)
+
+type ValidationResponse struct {
+	OkMessage    string `json:"okMessage"`
+	Code         string `json:"code"`
+	Advice       string `json:"advice,omitempty"` // eg; an exiting entry already contains filed ""
+	ErrorMessage string `json:"error,omitempty"`
+	IsError      bool   `json:"isError"`
+}
+
+func main() {
+	client, err := unixclient.NewUnixClient("/tmp/unix.sock")
+	if err != nil {
+		fmt.Println("Error creating client:", err)
+		return
+	}
+	validationResponse := &ValidationResponse{}
+	resp, err := client.Send("validation/ip", "192.168.", 5, &validationResponse, "any")
+	Print(resp)
+	Print(validationResponse)
+
+	client.Close()
+}
+
+func Print(i interface{}) {
+	fmt.Printf("%+v\n", i)
+	return
+}
